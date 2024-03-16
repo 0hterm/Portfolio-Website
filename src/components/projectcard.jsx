464a334fbd8b7524609435a3ff4bbd3b56fd1e88
0hterm <huntertermo@yahoo.com>
2024-03-15 23:10:52 -0400

    added proj.

---
+++ src/components/projectcard.jsx
@@ -0,0 +1,21 @@
+const ProjectCard = (props) => {
+    
+    const { title, description, img, gitLink, appLink } = props;
+
+    return(
+        <div className="card">
+            <h2>{title}</h2>
+                <img className="project-img" src={img} width="275px" alt="Project Image" />
+                <p className="project-desc">{description}</p>
+                <div className="buttons">
+                    <a href={gitLink} target="_blank"><button>GitHub Repository</button></a>
+                    { appLink ?
+                        <a href={appLink} target="_blank"><button>Use the App!</button></a>
+                        : null
+                    }
+                </div>
+        </div>
+    )
+}
+
+export default ProjectCard;
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,23 +1,11 @@
+import ProjectCard from "./projectcard";
+
 const Projects = () => {
     return (
         <div className="projects-container">
-            <div className="card">
-                <h2>PDF Document Chatbot</h2>
-                <img className="project-img" src="https://i.imgur.com/VIAHhXo.png" width="275px" alt="Project Image" />
-                <p className="project-desc">Skills Used: Python, OpenAI, langchain, chromaDB, and streamlit.</p>
-                <div className="buttons">
-                    <a href="https://github.com/0hterm/Streamlit_PDF_Chatter" target="_blank"><button>GitHub Repository</button></a>
-                    <a href="https://gpt-with-pdf.streamlit.app" target="_blank"><button>Use the App!</button></a>
-                </div>
-            </div>
-            <div className="card">
-                <h2>Pokedex Discord Bot</h2>
-                <img className="project-img" src="https://i.imgur.com/38NLyeu.png" width="275px" alt="Project Image" />
-                <p className="project-desc">Skills Used: Node.js, Discord.js, APIs, and working with JSON.</p>
-                <div className="buttons">
-                    <a href="https://github.com/0hterm/pokedex-bot" target="_blank"><button>GitHub Repository</button></a>
-                </div>
-            </div>
+            <ProjectCard title="PDF Document Chatbot" description="Skills Used: Python, OpenAI, langchain, chromaDB, and streamlit." img="https://i.imgur.com/VIAHhXo.png" gitLink="https://github.com/0hterm/Streamlit_PDF_Chatter" appLink="https://gpt-with-pdf.streamlit.app" />
+            <ProjectCard title="Pokedex Discord Bot" description="Skills Used: Node.js, Discord.js, APIs, and working with JSON." img="https://i.imgur.com/38NLyeu.png" gitLink="https://github.com/0hterm/pokedex-bot" />
+            <ProjectCard title="Pokemon Picker" description="Skills Used: Node.js, APIs and JSON data, Javascript, Algorithms, Data Structures." img="https://i.imgur.com/EkSPpWb.png" gitLink="https://github.com/COP4808-Spring2024-Full-Stack-Webdev/hw4-0hterm" />
         </div>
     )
 }
