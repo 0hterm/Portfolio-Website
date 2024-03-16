bff5665a2e50823a4163993859849ecfe47ff107
0hterm <huntertermo@yahoo.com>
2024-03-15 22:44:02 -0400

    contact page

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,6 +1,22 @@
 const Contact = () => {
     return (
         <div className="contact-container">
+            <div className="contact-i">
+                <img className="contact-img" src="https://static-00.iconduck.com/assets.00/linkedin-icon-512x512-dhkaf9ri.png" alt="LinkedIn:" />
+                <a href="https://www.linkedin.com/in/hunter-termo-006241143/">LinkedIn</a>
+            </div>
+            <div className="contact-i">
+                <img className="contact-img" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub:" />
+                <a href="https://github.com/0hterm">GitHub</a>
+            </div>
+            <div className="contact-i">
+                <img className="contact-img" src="https://uxwing.com/wp-content/themes/uxwing/download/communication-chat-call/phone-icon.png" alt="Phone #:" />
+                <a>(954)224-8781</a>
+            </div>
+            <div className="contact-i">
+                <img className="contact-img" src="https://static.vecteezy.com/system/resources/previews/021/454/514/non_2x/email-confirmation-app-icon-email-icon-free-png.png" alt="Email:" />
+                <a>huntertermo@yahoo.com</a>
+            </div>
         </div>
     )
 }
